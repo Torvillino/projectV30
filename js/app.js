ee01e320e26c687a210366df1f9b5b44ac3a183c
victor.manuel.jmz@gmail.com <victor.manuel.jmz@gmail.com>
2016-11-24 17:20:57 +0100

    Aplicacions la base de librerias y componentes

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// DECLARACION DE APP Y DEPENDENCIAS
+// ******************************
+var v30 = angular.module('v30', [
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngBonita',
+    'ngCookies',
+    'ngAria',
+    'ngAnimate',
+    'ngDialog'
+]);
---
+++ js/controller/MainController.js
@@ -0,0 +1,3 @@
+angular.module('v30').controller('MainController', function ($scope, $translate, $sce) {
+
+});
