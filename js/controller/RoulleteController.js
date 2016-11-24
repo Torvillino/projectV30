55b3b5542af0badb53cc4448ca687da13d095bb6
victor.manuel.jmz@gmail.com <victor.manuel.jmz@gmail.com>
2016-11-24 17:32:02 +0100

    background

---
+++ js/controller/RoulleteController.js
@@ -0,0 +1,35 @@
+//--------
+// 1.- Slycering
+//--------
+//murillo <--
+//demarco
+//garofalo
+//calleja
+//*
+
+//--------
+// 2.- Haffelpaff
+//--------
+//garcia <--
+//perez
+//mediavilla2
+//ortiz
+//rodriguez
+
+//--------
+// 3.- Gryfindor
+//--------
+//mediavilla <--
+//montilla
+//mayoral
+//martin2
+//iñigo
+
+//--------
+// 4.- Ravenclow
+//--------
+//muñoz <--
+//sola
+//madridano
+//iñigo2
+//martin
