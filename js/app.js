8a67a4015df9f7689468d88e5b1538212275a0ea
victor.manuel.jmz@gmail.com <victor.manuel.jmz@gmail.com>
2016-12-07 18:23:36 +0100

    Actualizamos

--- js/app.js
+++ js/app.js
@@ -1,12 +1,31 @@
 // DECLARACION DE APP Y DEPENDENCIAS
 // ******************************
-var v30 = angular.module('v30', [
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'ngBonita',
-    'ngCookies',
-    'ngAria',
-    'ngAnimate',
-    'ngDialog'
-]);
+var app = angular.module('vtools', []);
+
+app.controller("MainController", function($scope) {
+    
+	$scope.listUser = {"murillo":"ALPHA","demarco":"ALPHA","garofalo":"ALPHA","calleja":"ALPHA","garcia":"BETA","perez":"BETA","mediavilla":"BETA","ortiz":"BETA","rodriguez":"DELTA","mediavilla2":"DELTA","montilla":"DELTA","mayoral":"DELTA","martin2":"GAMMA","iñigo":"GAMMA","muñoz":"GAMMA","sola":"GAMMA","madridano":"SIGMA","iñigo2":"SIGMA","martin":"SIGMA"};
+	$scope.surname;
+	$scope.showFrat = false;
+	$scope.frat = "";
+
+	/**
+    * 
+    */
+    $scope.getFrat = function () {
+    	$scope.showFrat = true;
+        $scope.frat = $scope.listUser[$scope.surname];
+        console.log($scope.frat);
+    };
+
+
+    /**
+    * 
+    */
+    $scope.init = function () {
+        console.log('init');
+    };
+
+    $scope.init();
+
+});
--- js/controller/MainController.js
+++
@@ -1,3 +0,0 @@
-angular.module('v30').controller('MainController', function ($scope, $translate, $sce) {
-
-});
