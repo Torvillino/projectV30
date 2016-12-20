2b1edf39956ab7fdc1b990e1796670d62d646043
victor.manuel.jmz@gmail.com <victor.manuel.jmz@gmail.com>
2016-12-20 17:18:25 +0100

    restructuracion de equipos

--- js/app.js
+++ js/app.js
@@ -4,11 +4,72 @@ var app = angular.module('vtools', ['ngAnimate']);
 
 app.controller("MainController", function($scope) {
     
-	$scope.listUser = {"murillo":"1","demarco":"1","garofalo":"1","calleja":"1","garcia":"2","perez":"2","mediavilla":"2","ortiz":"2","rodriguez":"3","mediavilla2":"3","montilla":"3","mayoral":"3","martin2":"4","iñigo":"4","muñoz":"4","sola":"4","madridano":"5","iñigo2":"5","martin":"5"};
+	$scope.listUser = 
+        {
+            "madridano":"1",
+            "murillo":"1",
+            "iñigo2":"1",
+            "garofalo":"1",
+            "demarco":"1",
+            "garcia":"2",
+            "calleja":"2",
+            "mediavilla":"2",
+            "rodriguez":"2"
+            "montilla":"3",
+            "muñoz":"3",
+            "iñigo":"3",
+            "muñoz":"3",
+            "ortiz":"3",
+            "sola":"4",
+            "mayoral":"4",
+            "martinez":"4",
+            "perez":"4",
+            "carballo":"4"
+        };
+
+    $scope.listCaptain = 
+        {
+            "garcia":"1",
+            "madridano":"1",
+            "sola":"1",
+            "montilla":"1"
+        };
 	$scope.surname;
 	$scope.showFrat = false;
 	$scope.frat = "";
+    $scope.textCaptain = "Eres el lider";
 
+    // 1
+    //madridano (Eva)
+    //murillo (Felipe)
+    //iñigo2 (Pili)
+    //garofalo (Mau)
+    //demarco (Monica)
+
+    // 2
+    //garcia (David)
+    //calleja (Irene)
+    //mediavilla (Javi)
+    //rodriguez (Noa)
+    //rodriguez (Sandra)
+        
+    // 3
+    //montilla (Beatriz)
+    //muñoz (Eduardo)
+    //iñigo (Sergio)
+    //ortiz (Lorena)
+    //fernandez (aj)    
+
+    // 4
+    //sola (Fakundo)
+    //mayoral (Monica)
+    //martinez (Beatriz)
+    //perez (David)
+    //carballo (David)
+
+    //
+    //martin
+    //martin2
 
 	$scope.getFratKeyPress = function(keyEvent) {
 		if (keyEvent.which === 13){
@@ -27,21 +88,42 @@ app.controller("MainController", function($scope) {
 
     	// Recuperamos el apellido y realizamos un trim y un lowercase
     	// Para trabajar mas seguro
-    	var surnameKey = $scope.surname.toLowerCase().trim().replace(" ", "");
-
+    	var surnameKey = $scope.getCleanedString($scope.surname);
+        $scope.showFrat = true;
 		// Si la clave no es todosito accedemos al map
     	if(surnameKey != "todosito"){
     		// Comprobamos que existe el nombre
     		if($scope.listUser[surnameKey] != undefined){
     			$scope.frat = $scope.listUser[surnameKey];
-    			$scope.showFrat = true;
     		}else{
+
     			$scope.frat = '7';	
     		}
     	}else{
     		$scope.frat = '6';
     	}
     };
+
+    /**
+    * Metodo para saber si el usuario es capitan
+    */
+    $scope.isCaptain = function () {
+
+        var isCaptain = false;
+        var idUser = $scope.getCleanedString($scope.surname);
+
+        // Recorremos el map de capitanes
+        $.each($scope.listCaptain, function(key, value){
+            if(key == idUser){
+                isCaptain = true;
+            }
+        });
+
+        // Devolvemos si el usuario introducido es capitan
+        return isCaptain;
+
+    };
+
 
     /**
     * 
@@ -62,6 +144,34 @@ app.controller("MainController", function($scope) {
     };
 
 
+    /**
+    * Funciona para limpiar el strin de acentos caracteres raros
+    */
+    $scope.getCleanedString = function(cadena){
+       // Definimos los caracteres que queremos eliminar
+       var specialChars = "!@#$^&%*()+=-[]\/{}|:<>?,.";
+
+       // Los eliminamos todos
+       for (var i = 0; i < specialChars.length; i++) {
+           cadena= cadena.replace(new RegExp("\\" + specialChars[i], 'gi'), '');
+       }   
+
+       // Lo queremos devolver limpio en minusculas
+       cadena = cadena.toLowerCase();
+
+       // Quitamos espacios
+       cadena = cadena.replace(/ /g,"");
+
+       // Quitamos acentos y "ñ". Fijate en que va sin comillas el primer parametro
+       cadena = cadena.replace(/á/gi,"a");
+       cadena = cadena.replace(/é/gi,"e");
+       cadena = cadena.replace(/í/gi,"i");
+       cadena = cadena.replace(/ó/gi,"o");
+       cadena = cadena.replace(/ú/gi,"u");
+       cadena = cadena.replace(/ñ/gi,"n");
+
+       return cadena;
+    }
     /**
     * 
     */
